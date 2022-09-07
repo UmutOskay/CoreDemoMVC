eaac5ee0b6ddad8b85fbc9d651a45c68f00144b0
Umut Oskay <umut.oskay@ozu.edu.tr>
2022-09-07 11:41:51 +0300

    Add project files.

---
+++ CoreDemoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDemoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
