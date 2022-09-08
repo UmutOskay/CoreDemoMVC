9a693a1396ced36ab66fb4ab4680f91d77035311
Umut Oskay <umut.oskay@ozu.edu.tr>
2022-09-08 11:54:29 +0300

    Category Service, Manager is created and Entity framework repositories are implemented.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+      //  CategoryRepository categoryRepository = new CategoryRepository();
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public void CategoryAdd(Category category)
+        {
+            /*   if(category.CategoryName != "" && category.CategoryDescription != "" && category.CategoryName.Length>= 5 && category.CategoryStatus == true)
+               {
+                   categoryRepository.AddCategory(category);
+               }
+               else
+               {
+                   throw new NotImplementedException();
+               }*/
+            throw new NotImplementedException();
+
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            if(category.CategoryID != 0)
+            {
+                repo.Delete(category);
+            }
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
